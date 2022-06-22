fee089b5d8fc4c081957df5f7da1066d8f28c57b
adityasingh7402 <adityasingh7402@gmail.com>
2022-06-22 20:27:34 +0530

    after put function to add and remove cart

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
-import { RiArrowDownSLine } from "react-icons/ri";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { AiOutlineMinusCircle } from "react-icons/ai";
 
-const checkout = () => {
+const checkout = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
   return (<>
     <div className='catoBack flex'>
       <div className="checkout-title my-8 mx-5 w-full flex flex-row">
@@ -10,31 +11,22 @@ const checkout = () => {
           <p>Shopping List</p>
           <div className="products flex flex-col w-full text-sm mt-8">
             <div className="price-text flex justify-end px-4 text-base">Price</div>
-            <div className="product flex flex-row w-full border-t-2 border-b-2 border-gray-200 p-3">
+            {
+              Object.keys(cart).length==0 &&  <div className='flex justify-center text-4xl text-green-700 py-10 items-center border-t border-b border-gray-200'>Your cart is Empty...!</div>
+            }
+            {Object.keys(cart).map((k)=>{return <div key={k} className="product flex flex-row w-full border-t-2 border-b-2 border-gray-200 p-3">
               <div className="product-image w-1/6 flex justify-center">
-                <Image alt="ecommerce" className="block" src="/apple.png" width={150} height={150} />
+                <Image alt="ecommerce" className="block" src={cart[k].image} width={150} height={150} />
               </div>
               <div className="product-text w-2/3 flex justify-start flex-col">
-                <h1 className='text-2xl font-medium'>Fresh Red Apple (500gm)</h1>
-                <p className='text-lg'>Fruit</p>
+                <h1 className='text-2xl font-medium'>{cart[k].name}</h1>
+                <p className='text-lg'>{cart[k].variant}</p>
                 <p className='text-sm text-green-700 font-medium'>In stock</p>
                 <div className="impTool mt-4 flex flex-row">
-                  <div className="qytselet relative flex border-r border-gray-200 px-3">
-                    <select className="rounded border appearance-none cursor-pointer hover:bg-gray-100 border-gray-300 py-1 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-greenbg-green-700 text-base pl-5 pr-4">
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                      <option>4</option>
-                      <option>5</option>
-                      <option>6</option>
-                      <option>7</option>
-                      <option>8</option>
-                      <option>9</option>
-                      <option>10</option>
-                    </select>
-                    {/* <div className="downArrow absolute top-3 -mt-px text-gray-500 left-7 text-2xl">
-                      <RiArrowDownSLine />
-                    </div> */}
+                  <div className="qytselet relative flex border-r border-gray-200 px-3 items-center">
+                    <AiOutlineMinusCircle onClick={()=>{removeFromCart(k, 1, cart[k].price, cart[k].name, cart[k].variant, cart[k].image)}} className='text-3xl font-normal cursor-pointer mr-2' />
+                    <div className="qyantity w-11 rounded-sm text-center text-base h-7 border border-gray-600">{cart[k].qty}</div>
+                    <AiOutlinePlusCircle onClick={()=>{addToCart(k, 1, cart[k].price, cart[k].name, cart[k].variant, cart[k].image)}} className='text-3xl font-normal cursor-pointer ml-2' />
                   </div>
                   <div className="qytselet relative flex border-r border-gray-200 text-green-700 cursor-pointer hover:bg-gray-100 font-medium px-3 items-center">
                     <p>Delete</p>
@@ -45,8 +37,11 @@ const checkout = () => {
                 </div>
               </div>
               <div className="product-price w-1/6 flex justify-end">
-                <span className='text-2xl'>₹ 400.00</span>
+                <span className='text-2xl'>{cart[k].price}</span>
               </div>
+            </div>})}
+            <div className='flex justify-end'>
+              <button onClick={clearCart} className="flex text-white font-medium text-sm rounded-full bg-green-700 justify-center w-28 mt-5  py-2 hover:text-gray-800 hover:bg-white border transition-all border-green-700"><p>Clear Cart</p></button>
             </div>
           </div>
         </div>
@@ -55,7 +50,6 @@ const checkout = () => {
           <div className="subtotal text-xl text-gray-800 flex justify-end pt-3  border-b-2 border-gray-200">Delivery Charge : ₹40.00</div>
           <div className="subtotal text-3xl text-gray-800 flex justify-end pt-10">₹ 440.00</div>
           <div className="subtotal text-3xl text-gray-800 flex justify-end pt-3"><button className="flex text-white font-medium text-sm rounded-full bg-green-700 w-full justify-center  py-2 hover:text-gray-800 hover:bg-white border transition-all border-green-700"><p>Proceed to Buy</p></button></div>
-
         </div>
       </div>
     </div>
--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 import Image from 'next/image'
 import { useState } from 'react'
 
-const Post = () => {
+const Post = ({addToCart}) => {
   const router = useRouter()
   const { slug } = router.query
   const [pin, setPin] = useState()
@@ -30,36 +30,36 @@ const Post = () => {
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
             <div className="flex mb-4">
               <span className="flex items-center">
-                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-greenbg-green-700" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-greenbg-green-700" viewBox="0 0 24 24">
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-greenbg-green-700" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-greenbg-green-700" viewBox="0 0 24 24">
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-greenbg-green-700" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-greenbg-green-700" viewBox="0 0 24 24">
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-greenbg-green-700" viewBox="0 0 24 24">
+                <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-greenbg-green-700" viewBox="0 0 24 24">
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-greenbg-green-700" viewBox="0 0 24 24">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-greenbg-green-700" viewBox="0 0 24 24">
                   <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                 </svg>
                 <span className="text-gray-600 ml-3">4 Reviews</span>
               </span>
               <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
                 <a className="text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                   </svg>
                 </a>
                 <a className="text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                   </svg>
                 </a>
                 <a className="text-gray-500">
-                  <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                   </svg>
                 </a>
@@ -77,7 +77,7 @@ const Post = () => {
                     <option>4 piece</option>
                   </select>
                   <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4" viewBox="0 0 24 24">
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
                       <path d="M6 9l6 6 6-6"></path>
                     </svg>
                   </span>
@@ -86,7 +86,7 @@ const Post = () => {
             </div>
             <div className="flex items-center">
               <span className="title-font font-medium text-2xl text-gray-900 mr-5">$58.00</span>
-                <button className="flex text-white font-medium text-base rounded-full mx-2 bg-green-700 px-5 py-3 hover:text-gray-800 hover:bg-white border transition-all border-green-700"><p>Add to cart</p></button>
+              <button onClick={()=>{addToCart(slug, 1, 400, "Fresh Red Apple (500gm)", "Fruit", "/apple.png")}} className="flex text-white font-medium text-base rounded-full mx-2 bg-green-700 px-5 py-3 hover:text-gray-800 hover:bg-white border transition-all border-green-700"><p>Add to cart</p></button>
               <button className="flex text-white font-medium text-base rounded-full mx-2 bg-green-700 px-5 py-3 hover:text-gray-800 hover:bg-white border transition-all border-green-700"><p>Buy Now</p></button>
             </div>
 
